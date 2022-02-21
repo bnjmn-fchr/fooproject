67ff5c2fffeaa43391220be4ce8ef623992f5113
Benjamin Foucher <benjamin.foucher@gmail.com>
2022-02-21 12:24:05 +0100

    Initial Push

---
+++ fooproject/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    """
+    Find the right word !
+    """
+    color = input('What is the color of the white horse?')
+    if color == 'white':
+        return 'You did it! Very good! Dora would be proud.'
+    return 'Really?'
